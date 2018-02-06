79c387d1a344f36df55518363f59b5372abac069
xudongdong <2281221979@qq.com>
2018-02-06 23:10:28 +0800

    webpack color

--- build/prod.js
+++ build/prod.js
@@ -1,12 +1,28 @@
+
 var fs = require('fs-extra'),
   webpack = require('webpack'),
   PATHS = require('./config/PATHS'),
-  config = require('./webpack.prod.conf');
+  config = require('./webpack.prod.conf'),
+  chalk = require('chalk');
 
 fs.emptyDirSync(PATHS.DIST);
 fs.copySync(PATHS.STATIC, PATHS.DIST.join('static'));
 
 webpack(config, function(err, stats) {
   // show build info to console
-  console.log(stats.toString({ chunks: false, color: true }));
+  //console.log(stats.toString({ chunks: false, color: true }));
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n\n')
+
+  console.log(chalk.cyan('  Build complete.\n'))
+  console.log(chalk.yellow(
+    '  Tip: built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
+  ))
 });
